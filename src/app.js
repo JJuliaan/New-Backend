084e1572e100d9d8d1f48edf503f21a8cb0485b7
Julian <julyagueroa@gmail.com>
2023-08-22 18:18:28 -0300

    mongo

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ app.use(methodOverride('_method'))
 
 app.use(session({
     store: MongoStore.create({
-        mongoUrl: (process.env.MONGO_URL),
+        mongoUrl: 'mongodb+srv://admin:admin@backcurso.yvqog7v.mongodb.net/test?retryWrites=true&w=majority',
         mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true }
     }),
     secret: 'backSecret',
